fc18324a99c9753a1887c0ff85537d67f1a92c09
Derryck <oseiagyemanb@gmail.com>
2023-03-17 00:49:12 +0100

    update of add.php file

--- add.php
+++ add.php
@@ -6,11 +6,14 @@
     //     echo $_GET['specifications'];
     // }
 
+    include('config/db_connect.php');
+
     $email = $title = $specifications = '';
     $errors = array('email' => '', 'title' => '', 'specifications' => '' );
 
     if (isset($_POST['submit'])){
 
+        //email
         if(empty($_POST['email'])){
             $errors['email'] = 'Email field is reaquired <br />';
         } else {
@@ -20,6 +23,7 @@
             }
         }
 
+        //title
         if(empty($_POST['title'])){
             $errors['title'] = 'Title is required <br />';
         } else {
@@ -29,6 +33,7 @@
             }
         }
 
+        //specifications
         if(empty($_POST['specifications'])){
             $errors['specifications'] = 'At least one specification is required <br />';
         } else {
@@ -37,9 +42,33 @@
                 $errors['specifications'] = 'comma separated list';
             }
         }
+
+        //submitting the form and checking for errors
+        if(array_filter($errors)){
+            //echo 'errors in the form';
+        } else {
+            $email = mysqli_real_escape_string($conn, $_POST['email']);
+            $title = mysqli_real_escape_string($conn, $_POST['title']);
+            $specifications = mysqli_real_escape_string($conn, $_POST['specifications']);
+
+            //create sql
+            $sql = "INSERT INTO phones(title, email, specifications) VALUES('$title', '$email', '$specifications')";
+
+            //save to db and check
+            if(mysqli_query($conn, $sql)){
+                //success
+                header('Location: index.php');
+            } else {
+                //error
+                echo 'query error: ' . mysqli_error($error);
+            }
+
+            //redirecting to the homepage
+            //echo 'form is valid';
+            // header('Location: index.php');
+        }
     }
 
-    
 
 ?>
 
